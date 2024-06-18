0c8f6d6bf72530d922e315583e5016e19a97a3b8
yerimbet.aitzhanov <king_erosh@icloud.com>
2024-06-18 15:08:26 +0500

    v1.1 with Minimax

--- TicTacToe/Controller/TicTacToe.swift
+++ TicTacToe/Controller/TicTacToe.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 class TicTacToe {
-    
-    var arrayXO:[XO] = Array(repeating: XO(), count: 9)
+
+    var arrayXO: [XO] = Array(repeating: XO(), count: 9)
     var isGameComputer = false
     var counter = 0
     let winCombination = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
     var possibleIndecies = [0,1,2,3,4,5,6,7,8]
-    
+
     func choiceXO(index: Int) -> Int {
         if isGameComputer && counter != 0 {
             counter += 2
@@ -30,15 +30,15 @@ class TicTacToe {
         arrayXO[index].isWriteXO = true
         return counter
     }
-    
+
     func computerXO() -> Int {
-        let randomIndex = possibleIndecies.randomElement()!
-        possibleIndecies.remove(at: possibleIndecies.firstIndex(of: randomIndex)!)
-        arrayXO[randomIndex].name = "⭕️"
-        arrayXO[randomIndex].isWriteXO = true
-        return randomIndex
+        let bestMove = minimax(array: arrayXO, pos: possibleIndecies, maximizing: true).index
+        possibleIndecies.remove(at: possibleIndecies.firstIndex(of: bestMove)!)
+        arrayXO[bestMove].name = "⭕️"
+        arrayXO[bestMove].isWriteXO = true
+        return bestMove
     }
-    
+
     func win() -> String? {
         for i in winCombination {
             if arrayXO[i[0]].name == arrayXO[i[1]].name &&
@@ -52,11 +52,56 @@ class TicTacToe {
         }
         return nil
     }
-    
+
     func restart() {
         arrayXO = Array(repeating: XO(), count: 9)
         possibleIndecies = [0,1,2,3,4,5,6,7,8]
         counter = 0
     }
-    
+
+    func minimax(array: [XO], pos: [Int], maximizing: Bool) -> (score: Int, index: Int) {
+        if let score = evaluate(array: array) {
+            return (score, -1)
+        }
+        
+        var bestScore = maximizing ? Int.min : Int.max
+        var bestMove = -1
+
+        for p in pos {
+            var array2 = array
+            var pos2 = pos
+            pos2.remove(at: pos2.firstIndex(of: p)!)
+            array2[p].name = maximizing ? "⭕️" : "❎"
+            array2[p].isWriteXO = true
+
+            let currentScore = minimax(array: array2, pos: pos2, maximizing: !maximizing).score
+            
+            if maximizing {
+                if currentScore > bestScore {
+                    bestScore = currentScore
+                    bestMove = p
+                }
+            } else {
+                if currentScore < bestScore {
+                    bestScore = currentScore
+                    bestMove = p
+                }
+            }
+        }
+        return (bestScore, bestMove)
+    }
+
+    func evaluate(array: [XO]) -> Int? {
+        for i in winCombination {
+            if array[i[0]].name == array[i[1]].name &&
+                array[i[1]].name == array[i[2]].name &&
+                array[i[0]].isWriteXO {
+                return array[i[0]].name == "⭕️" ? 1 : -1
+            }
+        }
+        if !array.contains(where: { !$0.isWriteXO }) {
+            return 0 // Draw
+        }
+        return nil // Game is still ongoing
+    }
 }
